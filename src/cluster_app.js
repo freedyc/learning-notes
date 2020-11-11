892acc4dc2765dc40e1ee57362228fe4b7c8998c
dengyongchao <dengyongchao@126.com>
2020-11-12 00:14:14 +0800

    Fix

--- src/cluster_app.js
+++ src/cluster_app.js
@@ -10,11 +10,15 @@ if (cluster.isMaster) {
     cluster.on('exit', function(worker, code , signal) {
         console.log('worker process died, id', worker.process.pid);
     })
+    cluster.on('listening', function(worker, address) {
+        console.log(`A worker with  #${worker.id} is now connected to ${address.address}:${address.port}`)
+    });
 
 } else {
+    console.log(`Slave is ${process.pid}`);
     http.createServer((req, res) => {
         res.writeHead(200);
-        res.end('hello word');
-    }).listen(8000);    
+        res.end('Hello world!');
+    }).listen({ host: '127.0.0.1', port: 8000 });    
 }
 
